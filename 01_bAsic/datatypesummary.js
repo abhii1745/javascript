bb3d4aae4e5b84283e9fb26addc324b2547cff6e
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-06 18:29:34 +0000

    data summary

--- 01_bAsic/datatypesummary.js
+++ 01_bAsic/datatypesummary.js
@@ -9,3 +9,14 @@ const myfunction = function() {
     console.log("HEllow world");
     
 }
+let userOne =  { 
+    name : "abhishek",
+    email : "Abhishk@gmail.com"
+
+}
+let userTwo = userOne
+
+userTwo.email = "rahul@gmail.com"
+console.log(userOne);
+console.log(userTwo.email);
+
