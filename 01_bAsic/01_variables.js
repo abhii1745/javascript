8dcf0d210512b45f4a8406d0b4cb68d7556bd332
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-06 16:37:33 +0000

    datatypes

---
+++ 01_bAsic/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 420
+let accountEmail = "abhishek45@gmail.com"
+var accountPassword = "123456"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2 
+// console.log(accountId);
+
+accountEmail = "abhi123@gmail.com"
+console.log(accountEmail);
+
+accountPassword = "4444"
+console.log(accountPassword);
+
+accountCity = "mumbai"
+console.log(accountCity);
+
+
+
+console.table([accountEmail ,accountPassword, accountCity]);
+console.log(accountState);
+
+
+
---
+++ 01_bAsic/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+console.log(3+3);
+
+let name = "abhishek"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+
+//number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object 
+
+console.log(typeof state);
+console.log(typeof undefined);
+
