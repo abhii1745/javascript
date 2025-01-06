bb8924b677d8bdbed5c28859e56d6d46947eedaa
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-06 17:01:36 +0000

    conversion

---
+++ 01_bAsic/conversionOperational.js
@@ -0,0 +1,16 @@
+// console.log("Abhishek");
+let score = true
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+
+//"33"  => 33
+//"33abc"=> NaN
+
+let isLoggedIn = 1  //if we take 0 it will show false
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
