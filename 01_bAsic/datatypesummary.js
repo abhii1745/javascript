054f6aaf7cf65db29aa18ffea12eef58356888fb
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-06 18:09:14 +0000

    datatypesummary

---
+++ 01_bAsic/datatypesummary.js
@@ -0,0 +1,11 @@
+const players = ["Rohit","Dhawan","Kohli","Dhoni"]
+const myobj = {
+    name : "Abhishek",
+    age : 20,
+    city : "kalyan"
+
+}
+const myfunction = function() {
+    console.log("HEllow world");
+    
+}
