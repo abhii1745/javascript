7f0d0a8467b0da9be644c909e19e668fafdb12c1
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-07 11:21:58 +0000

    math

---
+++ 01_bAsic/06_num_and_math.js
@@ -0,0 +1,32 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(typeof balance.toString().length);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// const precision = 123.86896
+// console.log(precision.toPrecision(3));
+
+// const no = Number.MAX_SAFE_INTEGER
+// console.log(no);
+
+
+// **************************Math***********************
+// console.log(Math);
+// console.log(Math.abs(-4)); //it change negative number to postive number but positive number remain unchanged
+// console.log(Math.round(4.6)); 
+// console.log(Math.ceil(4.1)); 
+// console.log(Math.floor(4.9)); 
+
+// console.log((Math.random()*10 ) +1 );
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1)) +min );
+
+
