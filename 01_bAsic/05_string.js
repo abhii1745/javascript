4930f1f557ebf70e698793c4653a243cc7ac30e0
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-07 10:30:56 +0000

    string

---
+++ 01_bAsic/05_string.js
@@ -0,0 +1,24 @@
+const name = "abhishek is a good boy"
+const epoCount =  50
+
+// console.log(name +  epoCount + " value");
+// console.log(`hellow my name is ${name} and my repo count us ${epoCount}`);  //string 
+
+// console.log(name[0]);
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.charAt(2));
+
+// const newString = name.substring(0,6)
+// console.log(newString);
+
+// const anotherString = name.slice(-3, 8)  
+// console.log(anotherString);
+
+
+// const newStringOne = "          abhishek          "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// console.log(name.replace("h","r"));
+console.log(name.split(" "));
