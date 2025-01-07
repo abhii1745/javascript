0728b25cb953756b73eadd90662934e8b1baa85c
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-07 12:12:34 +0000

    dateTime

---
+++ 01_bAsic/07_dates_in_js.js
@@ -0,0 +1,12 @@
+//dates
+let date = new Date()
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleTimeString());
+//  let myCreateDate = new Date(2025,0,21)
+//  console.log(myCreateDate.toDateString());
+ 
+let newDate = new Date()
+console.log(newDate.getDay());
+console.log(newDate.getMonth());
