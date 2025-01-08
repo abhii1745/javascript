467f10b28d5899034c60a66171348fe47cb2347f
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-08 20:51:08 +0000

    scope

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,30 @@
+//  function one(){
+//     const username = "Abhishek"
+//     function Two(){
+//         const insta = "abhi45"
+//         console.log(username);
+//         function Three(){
+//             const ThirdFucntion = "HElloWorld"
+//         }
+//         // console.log(insta);
+//     }
+//     // console.log(insta);
+//     Two()
+    
+//  }
+// one()
+
+
+//////////////////////// interesting /////////////////
+function addOne(num){
+    return num +1 
+}
+console.log(addOne(5));
+
+
+const addTwo = function(num){
+   return num + 2
+}
+// console.log(addTwo(6));
+
+
