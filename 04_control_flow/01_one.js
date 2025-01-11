1b077ed89605d465256249ac6537797d57a84199
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-11 08:01:18 +0000

    if

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,39 @@
+// if else
+const number = 100
+if(number > 100 ){
+    console.log("Number is greater than 50");
+    
+} else if(number >= 101){
+console.log("number is eqaul to ");
+
+} else{
+
+console.log("numbers are not fall in criteriq ");
+
+}
+
+
+// switch operator
+
+switch (number) {
+    case 10:
+        console.log("number is 10");
+        
+        break;
+    case 20:
+        console.log("number is 20");
+        
+        break;
+    case 50:
+        console.log("number is 50");
+        
+        break;
+
+    default:
+        console.log("number not matched");
+        
+        break;
+}
+
+
+ 
