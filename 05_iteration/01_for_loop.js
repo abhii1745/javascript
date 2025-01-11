522c54b75e7dac36b26ffb486a864a87d1e67b89
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-11 10:48:32 +0000

    loop

---
+++ 05_iteration/01_for_loop.js
@@ -0,0 +1,66 @@
+//for
+// for (let index = 0 ; index < 10 ; index ++){
+//     const abhishek = index;
+//     if(abhishek == 5){
+//         console.log(`The number is 5`);
+        
+//     }
+//     console.log(abhishek);
+    
+// }
+
+
+
+
+
+// for(i = 0 ; i<10; i++){
+//     console.log(`outer loop ${i}`);
+//     for(j = 0 ; j <= 10 ; j ++){
+//         // console.log(`inner loop ${j}`);
+//         console.log(`${i} * ${j} = ${i*j}`);
+        
+//         // for(k = 0 ; k <= 11 ; k++){
+//         //     console.log(`last element k is ${k}`);
+            
+//         // }
+//     }
+    
+// }
+
+
+
+// print table from 1 to 20 
+// for(i = 1 ; i<=20; i++){
+//     console.log(`Table of ${i}`);
+//     for(j = 1 ; j <= 10 ; j ++){
+//         // console.log(`inner loop ${j}`);
+//         console.log(`${i} * ${j} = ${i*j}`);
+        
+//         // for(k = 0 ; k <= 11 ; k++){
+//         //     console.log(`last element k is ${k}`);
+            
+//         // }
+//     }
+    
+// }
+
+
+
+
+// for loop using array
+// const myArray = ["Abhishek","rahul","Vijay","Bheem"]
+// console.log(myArray.length);
+// for(i= 0; i<myArray.length; i++){
+//     const friends =  myArray[i]
+//     console.log(friends);
+    
+// }
+
+for(i = 0; i<=20; i++){
+    const num = i
+    if(num ==  16 ){
+        continue
+    }
+    console.log(num);
+    
+}
