6b49f5d42ba9243f96b8fad18d3fbb016aa31ac9
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-11 11:52:50 +0000

    do while

---
+++ 05_iteration/02_do_while.js
@@ -0,0 +1,30 @@
+// let num = 0
+// while (num <= 10){
+//     console.log(`number is ${num}`);
+//     num = num +2
+    
+
+// }
+
+
+
+// const myArray = ["Abhishek","rahul","Vijay","Bheem"]
+// let arr = 0
+// while(arr < myArray.length){
+//     let bros = myArray[arr]
+//     arr = arr+1
+//     console.log(`my friends are ${bros}`);
+    
+    
+// }
+
+
+
+
+
+// let score = 0
+// do {
+//     console.log(`score is ${score}`);
+//     score = score+1
+    
+// } while (score <=10);
