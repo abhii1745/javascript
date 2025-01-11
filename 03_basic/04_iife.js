a1dbfd42359fb6db1d63ffc61f3ac64d2435e53d
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-11 06:19:14 +0000

    iifie

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,10 @@
+(function iffie(){  //named iifie
+    console.log("database connected");
+    
+})();
+
+((name) => {  //unamed iffie/simple iffie 
+    console.log(`database connected two ${name}`);
+    
+})("abhishek")
+
