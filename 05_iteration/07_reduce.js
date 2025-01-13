19fff731f4e26b350bca73fcd5c9d82df47d9b8b
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-13 13:10:59 +0000

    dom

--- 05_iteration/07_reduce.js
+++ 05_iteration/07_reduce.js
@@ -1,6 +1,28 @@
-const myNums = [1,2,3]
-const newNums = myNums.reduce( function(acc,currVal){
-    return acc + currVal
-} ,0)
-console.log(newNums);
+// const myNums = [1,2,6]
+// // const newNums = myNums.reduce( function(acc,currVal){
+// //     return acc + currVal
+// // } ,0)
 
+// const newNums = myNums.reduce( (acc,currVal)=> (acc + currVal ),3)
+// console.log(newNums);
+
+const shoppingCart = [
+    {
+        courseName:"java",
+        price : 2999
+    },
+    {
+        courseName:"python",
+        price : 1999
+    },
+    {
+        courseName:"go",
+        price : 4999
+    },
+    {
+        courseName:"HTML",
+        price : 999
+    }
+]
+a = shoppingCart.reduce( (acc,item) => acc+item.price,0 )
+console.log(a);
