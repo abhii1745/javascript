042447080a82695af74d84b347e76b616357231b
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-18 18:53:27 +0000

    project

---
+++ 07_projects/05_UnlimitedColours/script.js
@@ -0,0 +1,13 @@
+const randomColor = function(){
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for(i = 0; i < 6; i ++){
+        color += hex[Math.floor(Math.random() * 16)]
+
+
+    }
+    return color;
+}
+console.log(randomColor());
+
+// console.log(Math.floor(Math.random() * 16));
