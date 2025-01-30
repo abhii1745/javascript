f905f866010ecb5967ce30dc3f68abec72f7ef28
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-30 18:13:26 +0000

    p2

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -12,6 +12,27 @@
 //     console.log("promise consumes");
     
 // })
+// new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         console.log("Async task 2");
+//         resolve()
+        
+//     },1000)
+// }).then(function(){
+//     console.log("promise 2 consumed");
+    
+
+// })
+
+
+
+
+
+
+
+
+
+
 
 
 
