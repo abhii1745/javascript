eb6e98b91b893bdf2c5239b561dda4c87b7d232e
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-30 18:12:27 +0000

    p1

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,22 @@
+// const promiseOne = new Promise(function(resolve, reject){
+
+//     setTimeout(function(){
+//         console.log("Async Await is completed");
+//         resolve()
+        
+//     },1000)
+
+
+// })
+// promiseOne.then(function(){
+//     console.log("promise consumes");
+    
+// })
+
+
+
+
+
+
+
+
