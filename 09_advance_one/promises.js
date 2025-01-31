82eae8a37bcbd80faa7f4478a02b8b240d755005
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-01-31 18:34:45 +0000

    06

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -102,3 +102,31 @@
     
 // }
 // consumePromiseFive()
+
+
+// async function HiteshChaudhary(){
+//     try {
+//         const response = await fetch("https://jsonplaceholder.typicode.com/users")
+//         const data = await response.json()
+//         console.log(data);
+        
+//     } catch (error) {
+//         console.log(error);
+        
+        
+//     }
+    
+// }
+// HiteshChaudhary()
+
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(function(response){
+    return response.json()
+}).then(function(data){
+    console.log(data);
+    
+}).catch(function(error){
+    console.log(error);
+    
+})
