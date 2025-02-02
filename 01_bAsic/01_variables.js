ef9555cc947b8c768bd6552fed3056d3c0292a4c
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-02-02 17:48:22 +0000

    pr

--- 01_bAsic/01_variables.js
+++ 01_bAsic/01_variables.js
@@ -1,25 +1,34 @@
-const accountId = 420
-let accountEmail = "abhishek45@gmail.com"
-var accountPassword = "123456"
-accountCity = "jaipur"
-let accountState;
+// const accountId = 420
+// let accountEmail = "abhishek45@gmail.com"
+// var accountPassword = "123456"
+// accountCity = "jaipur"
+// let accountState;
 
-// accountId = 2 
-// console.log(accountId);
+// // accountId = 2 
+// // console.log(accountId);
 
-accountEmail = "abhi123@gmail.com"
-console.log(accountEmail);
+// accountEmail = "abhi123@gmail.com"
+// console.log(accountEmail);
 
-accountPassword = "4444"
-console.log(accountPassword);
+// accountPassword = "4444"
+// console.log(accountPassword);
 
-accountCity = "mumbai"
-console.log(accountCity);
+// accountCity = "mumbai"
+// console.log(accountCity);
 
 
 
-console.table([accountEmail ,accountPassword, accountCity]);
-console.log(accountState);
+// console.table([accountEmail ,accountPassword, accountCity]);
+// console.log(accountState);
+
+
+let userInput = prompt("Enter a number:"); // Takes input as a string
+let number = parseInt(userInput); // Converts it to an integer
+
+console.log("You entered:", number);
+
+
+
 
 
 
