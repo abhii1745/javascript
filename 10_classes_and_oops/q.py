dd5c2def72d4ee26c766b8114dce5f524c062a90
Abhishek Yadav <143536044+abhii1745@users.noreply.github.com>
2025-02-04 17:08:52 +0000

    py

--- 10_classes_and_oops/q.py
+++ 10_classes_and_oops/q.py
@@ -10,3 +10,9 @@
 # lenght_of_seq = len(next)
 # while lenght_of_seq == 10:
 #     next = sequence + next_num
+
+#     print(sequence)
+# print(len(sequence))
+# for i in range (1,10):
+#     next = sequence + next_num
+#     print(next)
